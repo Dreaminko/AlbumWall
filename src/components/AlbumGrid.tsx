7cd0f9c56fff19cccaad9deb495a7fe4f0de222d
Dreaminko <Dream_ink@qq.com>
2026-07-15 16:56:45 +0800

    fix bottom gap

--- src/components/AlbumGrid.tsx
+++ src/components/AlbumGrid.tsx
@@ -15,7 +15,7 @@ export default function AlbumGrid({ albums }: AlbumGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-2 gap-4 px-4 pt-4 sm:grid-cols-3 lg:grid-cols-5">
+    <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 lg:grid-cols-5">
       {albums.map((album) => (
         <AlbumCard key={album.slug} album={album} />
       ))}
